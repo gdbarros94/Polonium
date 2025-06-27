bf288845a035fdf2bc3b674dabb94fde9a61cc86
Gabriel Barros <gdbarros95@gmail.com>
2025-06-27 19:51:00 -0300

    htacces with rewrite 2

--- bootstrap.php
+++ bootstrap.php
@@ -1,11 +1,8 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+
 // Carrega as configurações globais
 $config = require_once __DIR__ . 
 '/config/config.php';
-echo "teste";
 // Carrega os módulos principais
 require_once __DIR__ . '/core/System.php';
 require_once __DIR__ . '/core/ThemeHandler.php';
@@ -15,7 +12,11 @@ require_once __DIR__ . '/core/PluginHandler.php';
 require_once __DIR__ . '/core/DatabaseHandler.php';
 require_once __DIR__ . '/core/OutHandler.php';
 require_once __DIR__ . '/core/APIHandler.php';
-
+if($config['debug']){
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+}
 // Inicia o sistema
 System::init();
 ThemeHandler::init();
@@ -23,8 +24,6 @@ DatabaseHandler::init();
 OutHandler::init();
 PluginHandler::init();
 RoutesHandler::init();
-echo "teste 2";
 // Dispatch da rota
 RoutesHandler::dispatch();
-echo "teste 3";
 
