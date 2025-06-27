1df0ad468f44b4665cce2e823e836640ebdfaf79
Gabriel Barros <gdbarros95@gmail.com>
2025-06-27 19:51:20 -0300

    htacces with rewrite 2

--- core/RoutesHandler.php
+++ core/RoutesHandler.php
@@ -7,7 +7,6 @@ class RoutesHandler
 
     public static function init()
     {
-        echo "teste";
         // Rotas padrão do sistema
         self::addRoute("GET", "/", function () {
             echo "Bem-vindo ao CRM!";
