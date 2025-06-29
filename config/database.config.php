97eb41386227d5c190392054972e4f551df2d97c
LgZika <luizgustavodiaspulzoficial@gmail.com>
2025-06-29 13:41:41 -0300

    Inciando desenvolvimento da lógica de login e painel admin

--- config/database.config.php
+++ config/database.config.php
@@ -2,9 +2,9 @@
 return [
     'driver' => 'mysql',
     'host' => 'localhost',
-    'database' => 'crm_alunostds_dev_br',
-    'username' => 'crm_alunostds_dev_br',
-    'password' => '2A4Kwo1w2qehrqA',
+    'database' => 'core_crm',
+    'username' => 'root',
+    'password' => '',
     'charset' => 'utf8mb4',
     'collation' => 'utf8mb4_unicode_ci',
     'prefix' => 'tds_crm_v1_',
