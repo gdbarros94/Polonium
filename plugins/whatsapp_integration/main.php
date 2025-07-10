f09b2165afd1c43d30db032b886489058d849d41
Gabriel Barros <gdbarros95@gmail.com>
2025-07-10 19:13:02 -0300

    fix: corrigido caminho do include do webhook.php no plugin whatsapp_integration 3

--- plugins/whatsapp_integration/main.php
+++ plugins/whatsapp_integration/main.php
@@ -7,7 +7,7 @@
  */
 
 RoutesHandler::addRoute("POST", "/whatsapp/webhook", function() {
-    include './webhook.php';
+    include 'webhook.php';
 });
 
 RoutesHandler::addRoute("GET", "/whatsapp/webhook", function() {
