db694b49e3a9dabbbcd49b9f51acfc4a5a5debcb
Gabriel Barros <gdbarros95@gmail.com>
2025-07-11 19:59:36 -0300

    feat(admin/plugins): adiciona item Plugins no menu lateral do admin

--- plugins/system_manager/admin/admin.php
+++ plugins/system_manager/admin/admin.php
@@ -114,7 +114,7 @@ class SystemManagerAdmin {
             return;
         }
         $plugins = self::getAllPlugins();
-        include __DIR__ . '/templates/plugins.php';
+        include '/templates/plugins.php';
     }
 
     public static function pluginsUpload() {
